e45fc119a8dc7e9bcddcde8f7273a18bb6a87079
caitlinyo <caitlin.young@hotmail.com>
2022-02-08 20:21:32 +0800

    8 Feb
    8 Feb

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -5,7 +5,8 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.StoryView.as_view(), name='story')
+    path('<int:pk>/', views.StoryView.as_view(), name='story'),
+    path('add-story/', views.AddStoryView.as_view(),name='newStory'),
 
 ]
 
