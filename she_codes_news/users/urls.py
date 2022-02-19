c3e40da485a1f6c9b935259b2105e56e8c43b2ff
caitlinyo <caitlin.young@hotmail.com>
2022-02-19 10:37:27 +0800

    190222
    update

--- she_codes_news/users/urls.py
+++ she_codes_news/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import CreateAccountView
+from .views import CreateAccountView, ProfileView
 app_name = 'users'
 urlpatterns = [
     path('create-account/', CreateAccountView.as_view(), name='createAccount'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile')
     ]
-
--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -30,11 +30,15 @@ class CreateAccountView(CreateView):
 
 class ProfileView(LoginRequiredMixin, DetailView):
     model = CustomUser
-    template_name = 'users/userProfile.html'
+    template_name = 'users/ProfileView.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['latest_stories'] = NewsStory.objects.all()[:4]
-        context['all_stories'] = NewsStory.objects.all()
-        return context
+
+
+
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['latest_stories'] = NewsStory.objects.all()[:4]
+    #     context['all_stories'] = NewsStory.objects.all()
+    #     return context
 
